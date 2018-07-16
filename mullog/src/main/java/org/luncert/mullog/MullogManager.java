cc0350bbf1a1d9409e1c7c6499b7036d03fa46a7
HiQiQi428 <2725115515@qq.com>
2018-07-16 13:29:16 +0800

    ::

--- mullog/src/main/java/org/luncert/mullog/MullogManager.java
+++ mullog/src/main/java/org/luncert/mullog/MullogManager.java
@@ -3,7 +3,6 @@ package org.luncert.mullog;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
@@ -25,56 +24,52 @@ public class MullogManager implements Serializable {
 
     private static Map<String, Appender> appenders = new HashMap<>();
 
-    private static Path configPath;
-
     private MullogManager() {}
 
+    private static Path configPath;
+
     private static void loadConfig() {
-        // set config path
-        configPath = Paths.get(System.getProperty("user.dir"), "/src/main/resources/mullog.properties");
         // load config
-		try {
-			InputStream in = new FileInputStream(new File(configPath.toString()));
-            Map<String, Properties> confs = new HashMap<>();
-            Properties props = new Properties();
-            try {
-                props.load(in);
-                // cast
-                for (Object key : props.keySet()) {
-                    String tmp = (String)key;
-                    int i = tmp.lastIndexOf(".");
+        String configPath = MullogManager.class.getClassLoader().getResource("mullog.properties").toString().replace("file:", "");
+        MullogManager.configPath = Paths.get(configPath);
+        Map<String, Properties> confs = new HashMap<>();
+        Properties props = new Properties();
+        try {
+            InputStream in = new FileInputStream(new File(configPath));
+            props.load(in);
+            // cast
+            for (Object key : props.keySet()) {
+                String tmp = (String)key;
+                int i = tmp.lastIndexOf(".");
 
-                    String namespace = tmp.substring(0, i);
-                    String name = tmp.substring(i + 1);
-                    Object value = props.get(key);
+                String namespace = tmp.substring(0, i);
+                String name = tmp.substring(i + 1);
+                Object value = props.get(key);
 
-                    if (confs.containsKey(namespace)) confs.get(namespace).put(name, value);
-                    else {
-                        Properties subProps = new Properties();
-                        subProps.put(name, value);
-                        confs.put(namespace, subProps);
+                if (confs.containsKey(namespace)) confs.get(namespace).put(name, value);
+                else {
+                    Properties subProps = new Properties();
+                    subProps.put(name, value);
+                    confs.put(namespace, subProps);
+                }
+            }
+            // resolve
+            for (String namespace: confs.keySet()) {
+                Properties subProps = confs.get(namespace);
+                try {
+                    if (subProps.get("level") == null) throw new MullogException("field level must be specified in mullog.properties");
+                    if (subProps.get("type") == null) throw new MullogException("field type must be specified in mullog.properties");
+                    if (subProps.get("format") == null) throw new MullogException("field format must be specified in mullog.properties");
+                    Class<?> clazz = Class.forName(subProps.getProperty("type"));
+                    if (Appender.class.isAssignableFrom(clazz)) {
+                        Constructor<?> constructor = clazz.getConstructor(Properties.class);
+                        appenders.put(namespace, (Appender)constructor.newInstance(subProps));
                     }
+                } catch (ClassNotFoundException | IllegalAccessException | NoSuchMethodException | SecurityException | IllegalArgumentException | InvocationTargetException | InstantiationException e) {
+                    e.printStackTrace();
                 }
-                // resolve
-                for (String namespace: confs.keySet()) {
-                    Properties subProps = confs.get(namespace);
-                    try {
-                        if (subProps.get("level") == null) throw new MullogException("field level must be specified in mullog.properties");
-                        if (subProps.get("type") == null) throw new MullogException("field type must be specified in mullog.properties");
-                        if (subProps.get("format") == null) throw new MullogException("field format must be specified in mullog.properties");
-                        Class<?> clazz = Class.forName(subProps.getProperty("type"));
-                        if (Appender.class.isAssignableFrom(clazz)) {
-                            Constructor<?> constructor = clazz.getConstructor(Properties.class);
-                            appenders.put(namespace, (Appender)constructor.newInstance(subProps));
-                        }
-					} catch (ClassNotFoundException | IllegalAccessException | NoSuchMethodException | SecurityException | IllegalArgumentException | InvocationTargetException | InstantiationException e) {
-						e.printStackTrace();
-					}
-                }
-			} catch (IOException e) { e.printStackTrace(); }
-		} catch (FileNotFoundException e) {
-            e.printStackTrace();
-		}
+            }
+        } catch (IOException e) { e.printStackTrace(); }
     }
 
     static {
@@ -92,8 +87,6 @@ public class MullogManager implements Serializable {
         else return null;
     }
 
-    public static Path getConfigPath() {
-        return configPath;
-    }
+    public static Path getConfigPath() { return configPath; }
 
 }
