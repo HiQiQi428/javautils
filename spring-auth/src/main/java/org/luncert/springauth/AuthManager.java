461e4922ecba8de60d5c06020c52187ed81c8631
HiQiQi428 <2725115515@qq.com>
2018-08-27 10:06:51 +0800

    ::

--- spring-auth/src/main/java/org/luncert/springauth/AuthManager.java
+++ spring-auth/src/main/java/org/luncert/springauth/AuthManager.java
@@ -3,6 +3,9 @@ package org.luncert.springauth;
 public class AuthManager {
 
     public void grant(int accessLevel, Object user) {
+        if (accessLevel < 0 || user == null) {
+            throw new IllegalArgumentException("accessLevel < 0 or user is null");
+        }
         AuthInterceptor.grant(accessLevel, user);
     }
 
